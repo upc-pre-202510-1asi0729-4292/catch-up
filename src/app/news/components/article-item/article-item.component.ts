7a49617ac3c7d99701404b2969dd79b5bcdea881
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-26 15:42:11 -0500

    feat(articles): add article item component implementation.

--- src/app/news/components/article-item/article-item.component.ts
+++ src/app/news/components/article-item/article-item.component.ts
@@ -1,10 +1,23 @@
 import {Component, Input} from '@angular/core';
 import {Article} from '../../model/article.entity';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatCardModule} from '@angular/material/card';
+import {MatAnchor, MatIconButton} from '@angular/material/button';
+import {TranslatePipe} from '@ngx-translate/core';
+import {MatIcon} from '@angular/material/icon';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-article-item',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatAnchor,
+    TranslatePipe,
+    MatIconButton,
+    MatIcon,
+    MatSnackBarModule,
+    DatePipe
+  ],
   templateUrl: './article-item.component.html',
   styleUrl: './article-item.component.css'
 })
@@ -18,8 +31,22 @@ export class ArticleItemComponent {
       title: this.article.title,
       url: this.article.url
     };
+    if(navigator.share) {
+      try {
+        await navigator.share(articleShareInfo);
+        this.snackBar.open('Article shared successfully!', 'Close', { duration: 3000});
+      } catch (error) {
+        this.snackBar.open('Sharing failed.', 'Close', { duration: 3000});
+      }
+    } else {
+      try {
+        await navigator.clipboard.writeText(articleShareInfo.url);
+        this.snackBar.open('Article URL copied to clipboard.', 'Close', { duration: 3000});
+      } catch (error) {
+        this.snackBar.open('Failed to copy URL.', 'Close', { duration: 3000});
+      }
+    }
 
-    // TODO: Implement the share functionality
   }
 
 }
